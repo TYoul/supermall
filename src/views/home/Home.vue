46131470e00cec17b7dc751eaf6f215f7e7f5ab4
TYoul_glb <710916361_glb@qq.com>
2020-03-13 12:29:03 +0800

    第二天

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,48 +1,170 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommends="recommends"></recommend-view>
+
+    <scroll class="content" ref="scroll" 
+            :probe-type="3" 
+            @scroll="contentScroll"
+            :pull-up-load="true"
+            @pullingUp="loadMore">
+        <home-swiper :banners="banners"></home-swiper>
+        <recommend-view :recommends="recommends"></recommend-view>
+        <feature-view></feature-view>
+        <tab-control class="tab-control" 
+                  :titles="['流行','新款','精选']" 
+                  @tabClick='tabClick'></tab-control>
+        <good-list :goods="showGoods"></good-list>
+    </scroll>
+
+    <back-top @click.native="backClick" v-show="isShowBackTop"></back-top>
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
   import HomeSwiper from './childComps/HomeSwiper'
   import RecommendView from './childComps/RecommendView'
+  import FeatureView from './childComps/FeatureView'
 
+  import NavBar from 'components/common/navbar/NavBar'
+  import TabControl from  'components/content/tabControl/TabControl'
+  import GoodList from 'components/content/goods/GoodsList'
+  import Scroll from 'components/common/scroll/Scroll'
+  import BackTop from 'components/content/backTop/BackTop'
 
-  import {getHomeMultidata} from 'network/home'
-  
+  import {getHomeMultidata, getHomeGoods} from 'network/home'
   
 
 export default {
   name: 'Home',
   components: {
-    NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl,
+    GoodList,
+    Scroll,
+    BackTop
   },
   data () {
     return {
       banners:[],
-      recommends:[]  
+      recommends:[],
+      goods:{
+        'pop' :{page: 0, list: []},
+        'new':{page: 0, list: []},
+        'sell':{page: 0, list: []},
+      },
+      currentType: 'pop',
+      isShowBackTop: false  
     }
   },
-  created () {
+  created() {
     // 1.请求多个数据
-    getHomeMultidata().then(res=>{
+    this.getHomeMultidata()
+
+    // 2.请求商品数据
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  mounted () {
+    this.$refs.swiper
+  },
+  computed: {
+    showGoods(){
+      return this.goods[this.currentType].list
+    }
+  },
+  methods: {
+    /** 
+    * 事件监听相关的方法
+    */ 
+    tabClick(index){
+      switch (index) {
+        case 0:
+          this.currentType = 'pop'
+          break;
+        case 1:
+          this.currentType = 'new'
+          break;
+        case 2:
+          this.currentType = 'sell'
+      }
+    },
+    backClick(){
+      this.$refs.scroll.scrollTo(0, 0)
+    },
+    contentScroll(position){
+      // position.y > 1000
+      this.isShowBackTop = -position.y > 1000
+    },
+    loadMore(){
+      this.getHomeGoods(this.currentType)
+    },
+    /** 
+    * 网络请求相关的方法
+    */
+    getHomeMultidata(){
+      getHomeMultidata().then(res=>{
       // this.result = res;
       this.banners = res.data.banner.list
       this.recommends = res.data.recommend.list
     })
+    },
+
+    getHomeGoods(type){
+      const page = this.goods[type].page + 1
+      getHomeGoods(type,page).then(res=>{
+      this.goods[type].list.push(...res.data.list)
+      this.goods[type].page += 1
+      
+      this.$refs.scroll.finishPullUp()
+    })
+    }
   }
 }
 </script>
 
 <style scoped>
+  #home{
+    padding-top: 44px;
+    height: 100vh;
+    position: relative;
+  }
+
   .home-nav{
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control{
+    /* position: sticky; */
+    top: 44px;
+    background-color: #fff;
+    z-index: 9;
+  }
+
+  /* .content{
+    height: calc(100%-93px);
+    overflow: hidden;
+    margin-top: 44px;
+  } */
+
+  .content{
+    /* height: 300px; */
+    overflow: hidden;
+
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
   }
 </style>
